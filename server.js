ecf9fa4d4df0e3058f64f4f2a7e36458c714b5a5
roger <roogerbrito1@gmail.com>
2023-02-20 07:53:03 -0300

    alteração de texto do app.listen

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import app from './app';
 const port = 3001;
 app.listen(port, () => {
   console.log();
-  console.log(`Escutando na porta ${port}`);
+  console.log(`Executando na porta ${port}`);
   console.log(`CTRL + Clique em http://localhost:${port}`);
 });
